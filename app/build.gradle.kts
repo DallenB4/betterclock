172995f39fbd01cb8a851bc09ebc4ae0e77fe54a
Snakehater <vigor.turujlija@gmail.com>
2025-08-04 19:12:47 +0200

    sign release apks

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,18 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+       create("release") {
+         storeFile = file("release.keystore")
+         storePassword = System.getenv("KEYSTORE_PASSWORD")
+         keyAlias    = System.getenv("KEY_ALIAS")
+         keyPassword = System.getenv("KEY_PASSWORD")
+       }
+    }
+
     buildTypes {
         release {
+	    signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
